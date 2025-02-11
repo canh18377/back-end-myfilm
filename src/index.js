43fe483b719fb0f9673af98feb3924d97b2cbc17
canh18377 <canh10101101@gmail.com>
2025-02-11 23:37:01 +0700

    sua

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
-const cors = require("cors");
-const router = require("./routes/index");
-const { engine } = require("express-handlebars");
-var bodyParser = require("body-parser");
-const db = require("./config/connect");
-const app = express();
-const port = 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors()); // Cho phép tất cả các yêu cầu CORS
-
-app.use(morgan("combined"));
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "resourse", "views"));
-
-//path folder store static file
-app.use(express.static(path.join(__dirname, "public")));
-
-//middelware đọc json từ client
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-router(app);
-db.connect();
-app.listen(port, "0.0.0.0", () => {
-  console.log(`App listening on port ${port}`);
+app.get("/", (req, res) => {
+  res.send("Backend TikTok API is running...");
 });
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
